14a58dc268540d4b00f7f0ba0a6149d2cc6f892c
Julian Petric <jpetric@folderit.net>
2020-09-16 11:03:04 -0300

    Implementación recursos y splash screen android

--- FolderAppXamarin/FolderApp.Android/MainActivity.cs
+++ FolderAppXamarin/FolderApp.Android/MainActivity.cs
@@ -1,96 +1,20 @@
 ﻿using Android.App;
 using Android.Content.PM;
-using Android.Runtime;
 using Android.OS;
-using Android.Gms.Common;
-using Android.Util;
-using System;
 
 namespace FolderApp.Droid
 {
-    [Activity(Label = "FolderApp", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "FolderApp", Icon = "@mipmap/icon", Theme = "@style/MainTheme", ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
 
         public const string TAG = "MainActivity";
-        internal static readonly string CHANNEL_ID = "my_notification_channel";
 
         protected override void OnCreate(Bundle savedInstanceState)
         {
-
-            Window.SetStatusBarColor(Android.Graphics.Color.Black);
-
-            if (Intent.Extras != null)
-            {
-                foreach (var key in Intent.Extras.KeySet())
-                {
-                    if (key != null)
-                    {
-                        var value = Intent.Extras.GetString(key);
-                        Log.Debug(TAG, "Key: {0} Value: {1}", key, value);
-                    }
-                }
-            }
-
-            if (!IsPlayServicesAvailable())
-            {
-                throw new Exception("This device does not have Google Play Services and cannot receive push notifications.");
-            }
-            CreateNotificationChannel();
-
-            TabLayoutResource = Resource.Layout.Tabbar;
-            ToolbarResource = Resource.Layout.Toolbar;
-
             base.OnCreate(savedInstanceState);
-
-            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
-            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
-            global::Xamarin.Forms.FormsMaterial.Init(this, savedInstanceState);
+            Window.SetStatusBarColor(Android.Graphics.Color.Black);
             LoadApplication(new App());
-       }
-        
-        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
-        {
-            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
-
-            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
-        }
-
-        public bool IsPlayServicesAvailable()
-        {
-            int resultCode = GoogleApiAvailability.Instance.IsGooglePlayServicesAvailable(this);
-            if (resultCode != ConnectionResult.Success)
-            {
-                if (GoogleApiAvailability.Instance.IsUserResolvableError(resultCode))
-                    Log.Debug(TAG, GoogleApiAvailability.Instance.GetErrorString(resultCode));
-                else
-                {
-                    Log.Debug(TAG, "This device is not supported");
-                    Finish();
-                }
-                return false;
-            }
-
-            Log.Debug(TAG, "Google Play Services is available.");
-            return true;
-        }
-
-        void CreateNotificationChannel()
-        {
-            // Notification channels are new as of "Oreo".
-            // There is no need to create a notification channel on older versions of Android.
-            if (Build.VERSION.SdkInt >= BuildVersionCodes.O)
-            {
-                var channelName = AppConstants.NotificationChannelName;
-                var channelDescription = String.Empty;
-                var channel = new NotificationChannel(channelName, channelName, NotificationImportance.Default)
-                {
-                    Description = channelDescription
-                };
-
-                var notificationManager = (NotificationManager)GetSystemService(NotificationService);
-                notificationManager.CreateNotificationChannel(channel);
-            }
         }
     }
 }
---
+++ FolderAppXamarin/FolderApp.Android/SplashActivity.cs
@@ -0,0 +1,86 @@
+﻿using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.OS;
+using Android.Gms.Common;
+using Android.Util;
+using System;
+using Android.Content;
+
+namespace FolderApp.Droid
+{
+    [Activity(Label = "FolderApp", MainLauncher = true, Icon = "@mipmap/icon", Theme = "@style/SplashTheme", ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class SplashActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        public const string TAG = "SplashACtivity";
+        Bundle savedInstance { get; set; }
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            savedInstance = savedInstanceState;
+        }
+
+        protected override void OnResume()
+        {
+            base.OnResume();
+            if (!IsPlayServicesAvailable())
+            {
+                throw new Exception("This device does not have Google Play Services and cannot receive push notifications.");
+            }
+            CreateNotificationChannel();
+
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            Xamarin.Essentials.Platform.Init(this, savedInstance);
+            global::Xamarin.Forms.Forms.Init(this, savedInstance);
+            global::Xamarin.Forms.FormsMaterial.Init(this, savedInstance);
+            StartActivity(new Intent(Application.Context, typeof(MainActivity)));
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+
+        public bool IsPlayServicesAvailable()
+        {
+            int resultCode = GoogleApiAvailability.Instance.IsGooglePlayServicesAvailable(this);
+            if (resultCode != ConnectionResult.Success)
+            {
+                if (GoogleApiAvailability.Instance.IsUserResolvableError(resultCode))
+                    Log.Debug(TAG, GoogleApiAvailability.Instance.GetErrorString(resultCode));
+                else
+                {
+                    Log.Debug(TAG, "This device is not supported");
+                    Finish();
+                }
+                return false;
+            }
+
+            Log.Debug(TAG, "Google Play Services is available.");
+            return true;
+        }
+
+        void CreateNotificationChannel()
+        {
+            // Notification channels are new as of "Oreo".
+            // There is no need to create a notification channel on older versions of Android.
+            if (Build.VERSION.SdkInt >= BuildVersionCodes.O)
+            {
+                var channelName = AppConstants.NotificationChannelName;
+                var channelDescription = String.Empty;
+                var channel = new NotificationChannel(channelName, channelName, NotificationImportance.Default)
+                {
+                    Description = channelDescription
+                };
+
+                var notificationManager = (NotificationManager)GetSystemService(NotificationService);
+                notificationManager.CreateNotificationChannel(channel);
+            }
+        }
+    }
+}
